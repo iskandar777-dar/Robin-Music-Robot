15b4d8807c19548d983c07fdb2b095893a52876a
OTAKU • BINGE <113184521+otakubinge@users.noreply.github.com>
2022-09-13 19:19:56 +0530

    Update video.py

--- RobinMusic/Player/video.py
+++ RobinMusic/Player/video.py
@@ -2,13 +2,13 @@ import re
 import asyncio
 
 from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2, IMG_6
-from RobinMusic.inline import stream_markup
+from NIXA.inline import stream_markup
 from Process.design.thumbnail import thumb
 from Process.design.chatname import CHAT_TITLE
 from RobinMusic.filters import command, other_filters
-from RobinMusic.queues import QUEUE, add_to_queue
-from RobinMusic.main import call_py, Test as user
-from RobinMusic.main import bot as Client
+from .queues import QUEUE, add_to_queue
+from NIXA.main import call_py, Test as user
+from NIXA.main import bot as Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pytgcalls import StreamType
@@ -20,7 +20,7 @@ from pytgcalls.types.input_stream.quality import (
     MediumQualityVideo,
 )
 from youtubesearchpython import VideosSearch
-IMAGE_THUMBNAIL = "https://telegra.ph/file/db09d094f55e3508bc172.jpg"
+IMAGE_THUMBNAIL = "https://te.legra.ph/file/7efcc30c166d793ed98a5.jpg"
 
 
 def ytsearch(query: str):
@@ -182,8 +182,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("👥 sᴜᴘᴘᴏʀᴛ", url="https://t.me/kakashi_bots_support"),
-                            InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/kakashi_bots_updates")
+                            InlineKeyboardButton("👥 sᴜᴘᴘᴏʀᴛ", url="https://t.me/TechQuardSupport"),
+                            InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/TechQuard")
                         ],
                         [
                             InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="cls")
@@ -259,8 +259,8 @@ async def vplay(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("👥 sᴜᴘᴘᴏʀᴛ", url="https://t.me/kakashi_bots_support"),
-                            InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/kakashi_bots_updates")
+                            InlineKeyboardButton("👥 sᴜᴘᴘᴏʀᴛ", url="https://t.me/TechQuardSupport"),
+                            InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/TechQuard")
                         ],
                         [
                             InlineKeyboardButton(" ᴄʟᴏsᴇ ", callback_data="cls")
